e531a7588db286f464c085ba7af8a06b42830f11
slamSIO <slam@siolla.fr>
2018-10-01 14:26:56 +0200

    Classe ModeleGsb

---
+++ ModeleGsb.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List ;
+
+public class ModeleGsb {
+
+	private static ModeleGsb modele = null ;
+	
+	private List<Visiteur> lesVisiteurs = new ArrayList<Visiteur>() ;
+	
+	private ModeleGsb(){
+		super() ;
+		this.peupler() ;
+	}
+	
+	public static ModeleGsb getInstance(){
+		if( ModeleGsb.modele == null ){
+			modele = new ModeleGsb() ;
+		}
+		return ModeleGsb.modele ;
+	}
+	
+	private void peupler(){
+		this.lesVisiteurs.add( new Visiteur("a131","azerty","Villechalane","Louis") ) ;
+		this.lesVisiteurs.add( new Visiteur("b13","azerty","Bentot","Pascal") ) ;
+		this.lesVisiteurs.add( new Visiteur("b16","azerty","Bioret","Luc") ) ;
+		this.lesVisiteurs.add( new Visiteur("a17","azerty","Andre","David") ) ;
+	}
+	
+	public Visiteur seConnecter(String matricule, String mdp){
+		
+		for( Visiteur unVisiteur : this.lesVisiteurs ){
+			if( unVisiteur.getMatricule().equals(matricule) && unVisiteur.getMdp().equals(mdp) ){
+				return unVisiteur ;
+			}
+		}
+		
+		return null ;
+		
+	}
+}
